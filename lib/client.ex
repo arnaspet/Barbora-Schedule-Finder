71ab0a671b761b387707934fbd9fd45cf733ae6f
Arnas Petruškevičius <arnas@tesonet.com>
2020-03-25 21:25:59 +0200

    Fix barbora's client which broke after new login changes

--- lib/client.ex
+++ lib/client.ex
@@ -2,6 +2,8 @@ defmodule Barbora.Client do
   use Tesla
   require Logger
 
+#  plug Tesla.Middleware.Logger
+
   def get_deliveries(client) do
     get!(client, "/api/eshop/v1/cart/deliveries")
   end
@@ -13,7 +15,10 @@ defmodule Barbora.Client do
       Tesla.Middleware.FormUrlencoded,
       Tesla.Middleware.DecodeJson,
       # fun fact: this header must be set :D which decodes to: apikey:SecretKey
-      {Tesla.Middleware.Headers, [{"Authorization", "Basic YXBpa2V5OlNlY3JldEtleQ=="}]}
+      {Tesla.Middleware.Headers, [
+        {"Authorization", "Basic YXBpa2V5OlNlY3JldEtleQ=="},
+        {"User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36}"},
+      ]}
     ]
 
     client = Tesla.client(middleware)
@@ -30,6 +35,9 @@ defmodule Barbora.Client do
   end
 
   defp get_login_cookie(client, {email, password}) do
+    %Tesla.Env{headers: headers} = get!(client, "")
+    cookie = generate_cookie([{"set-cookie", "region=barbora.lt"} | headers])
+
     case post!(
            client,
            "/api/eshop/v1/user/login",
@@ -37,13 +45,14 @@ defmodule Barbora.Client do
              email: email,
              password: password,
              rememberMe: true
-           }
+           },
+             headers: [{"Cookie", cookie}]
          ) do
       %Tesla.Env{status: 200, headers: headers} ->
         {:ok, generate_cookie(headers)}
 
-      %Tesla.Env{status: status, body: body} ->
-        Logger.info("Request failed: #{inspect(body)}")
+      %Tesla.Env{status: status} = err ->
+        Logger.info("Request failed: #{inspect(err)}")
         {:error, status}
     end
   end
