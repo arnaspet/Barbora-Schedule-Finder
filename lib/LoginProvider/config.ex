c453dff77e30d044676f34f839d26d214a04a71a
Arnas Petruškevičius <arnas@tesonet.com>
2020-04-01 10:05:45 +0300

    Remove unused modules

--- lib/LoginProvider/config.ex
+++
@@ -1,6 +0,0 @@
-defmodule Barbora.LoginProvider.Config do
-  def provide() do
-    {Application.fetch_env!(:barbora, Barbora.LoginProvider.Config)[:email],
-     Application.fetch_env!(:barbora, Barbora.LoginProvider.Config)[:password]}
-  end
-end
